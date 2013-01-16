8962875379c8be9cf757d099e3a838ae067e3c25
Jan Wielemaker <J.Wielemaker@cs.vu.nl>
2013-01-16 10:21:29 +0100

    WIP on repeating patterns

--- labels.pl
+++ labels.pl
@@ -42,3 +42,32 @@ same(H, C0, C, [H|T0], T) :- !,
 	C1 is C0+1,
 	same(H, C1, C, T0, T).
 same(_, C, C, T, T).
+
+%%	repeating_pattern(+List, -RepDesc) is det.
+%
+%	Represent List fragments using repeat(Count, Seq).  E.g.
+%
+%	  ==
+%	  ?- repeating_pattern([a,b,c,b,c,d], P).
+%	  P is [repeat(1,[a]), repeat(2,[b,c]), repeat(1,[d])].
+%	  ==
+
+repeating_pattern([], []) :- !.
+repeating_pattern(List, [repeat(Rep,Pat0)|Pattern]) :-
+	rappend(Pat0, Rest0, List),
+	Pat0 \== [],
+	append(Pat0, Rest1, Rest0), !,
+	repeat(Pat0, Rest1, 2, Rep, Rest1),
+	repeating_pattern(Rest1, Pattern).
+repeating_pattern([H|T], [repeat(1,H)|Pattern]) :-
+	repeating_pattern(T, Pattern).
+
+repeat(Pat, List, Rep0, Rep, Rest) :-
+	append(Pat, Rest0, List), !,
+	Rep1 is Rep0+1,
+	repeat(Pat, Rest0, Rep1, Rep, Rest).
+repeat(_, List, Rep, Rep, List).
+
+rappend([H|T], L, [H|R]) :-
+	rappend(T, L, R).
+rappend([], L, L).
